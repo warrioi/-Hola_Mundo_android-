d810fff44343a833a7a287aff1857590a7b81761
unknown <you@example.com>
2021-04-22 12:39:35 -0400

    Mi primer commit

---
+++ app/src/main/java/com/example/holamundo/Hola_Mundo_android.java
@@ -0,0 +1,14 @@
+package com.example.holamundo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Hola_Mundo_android extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hola__mundo_android);
+    }
+}
